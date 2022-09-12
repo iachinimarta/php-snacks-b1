1b42aa303e945ced2d22e3088d2ec499f2042d7f
Marta Iachini <iachinimarta@gmail.com>
2022-09-12 16:13:02 +0200

    Esercizio #2 completato

---
+++ ## Snack 2/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = $_GET['name'];
+        $mail = $_GET['mail'];
+        $age = $_GET['age'];
+        
+    ?>
+
+    <p>
+        <?php
+            if (strlen($name) >= 3 && $mailCont = strpos($mail, '@') !== false && $mailCont = strpos($mail, '.') !== false && is_numeric($age)) {
+                echo 'Accesso riuscito!' . '<br>' . $name . '<br>' . $mail;
+            } else {
+                echo 'Accesso negato!';
+            }
+        ?>
+    </p>
+</body>
+</html>
+
